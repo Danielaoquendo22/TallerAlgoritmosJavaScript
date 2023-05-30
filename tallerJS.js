c70bf6c4c3f8a0d299a589dd87cd4d0528899085
Daniela Oquendo Tabares <DanielaOquendo@comfama.com.co>
2023-05-30 15:03:28 -0500

    primer punto del taller

---
+++ tallerJS.js
@@ -0,0 +1,43 @@
+let num1 = parseFloat(prompt("Ingrese el primer número:"));
+let num2 = parseFloat(prompt("Ingrese el segundo número:"));
+let num3 = parseFloat(prompt("Ingrese el tercer número:"));
+let mayor
+let menor
+let medio
+
+if(num1 > num2 && num1 >num3){
+    mayor = num1
+    
+    if (num2 > num3){
+        medio = num2
+        menor = num3
+    }else{
+        medio = num3
+        menor = num2
+    }
+}
+
+if(num2 > num1 && num2 >num3){
+    mayor = num2
+    if (num1 > num3){
+        medio = num1
+        menor = num3
+    }else{
+        medio = num3
+        menor = num1
+    }
+}
+
+if(num3 > num1 && num3 >num2){
+    mayor = num3
+    if (num1 > num2){
+        medio = num1
+        menor = num2
+    }else{
+        medio = num2
+        menor = num1
+    }
+}
+
+console.log ("Este es el orden de menor a mayor " +  menor +" , "+ medio +" , "+ mayor)
+console.log ("Este es el orden de mayor a menor  " +  mayor +" , "+ medio +" , "+ menor)

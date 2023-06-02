5aaeb8595d0641dd67f66a7749e4635c0f83fd8c
Daniela Oquendo Tabares <DanielaOquendo@comfama.com.co>
2023-06-01 21:02:29 -0500

    menu taller Js

--- tallerJS.js
+++ tallerJS.js
@@ -1,4 +1,6 @@
 //punto 1
+
+function uno(){
 let num1 = parseFloat(prompt("Ingrese el primer número:"));
 let num2 = parseFloat(prompt("Ingrese el segundo número:"));
 let num3 = parseFloat(prompt("Ingrese el tercer número:"));
@@ -43,7 +45,11 @@ if(num3 > num1 && num3 >num2){
 console.log ("Este es el orden de menor a mayor " +  menor +" , "+ medio +" , "+ mayor)
 console.log ("Este es el orden de mayor a menor  " +  mayor +" , "+ medio +" , "+ menor)
 
+}
+
 //punto 2
+
+function dos(){
 
 let perimetro =parseFloat(prompt("Ingresa el valor del perimetro"));
 let radio  = parseFloat(prompt("Ingresa el calor del radio"));
@@ -58,9 +64,11 @@ area= (rdoPerimetro*radio)/2;
 
 
 console.log("El area del circulo es de:" + area + " " + "cm2");
-
+}
 
 //punto 3
+
+function tres(){
 
 let ladoUno= parseFloat(prompt("Ingrese el valor del lado uno: "));
 let ladoDos= parseFloat(prompt("Ingrese el valor del lado dos: "));
@@ -69,13 +77,15 @@ let ladoTres= parseFloat(prompt("Ingrese el valor del lado tres: "));
 if (ladoUno == ladoDos && ladoUno ==ladoTres && ladoDos == ladoTres){
     console.log("Es un triangulo Equilatero")      
 }else{
-    if(ladoUno !=ladoDos && ladoUno !=ladoTres && ladoDos == ladoTres){
+    if((ladoUno !=ladoDos && ladoUno !=ladoTres && ladoDos == ladoTres)||(ladoDos !=ladoUno && ladoDos!=ladoTres && ladoUno == ladoTres)||(ladoTres !=ladoDos && ladoTres !=ladoUno && ladoDos == ladoUno)){
         console.log("Es un triangulo Isosceles")
     }else{
         if (ladoUno !=ladoDos && ladoUno!= ladoTres && ladoDos != ladoTres)
             console.log("Es un triangulo Escaleno")
         }
     }
+
+}
 
 //  punto 4 
 
@@ -103,14 +113,7 @@ function calDivisor(numero) {
 
   }
   
-  let numero1 = parseInt(prompt("Ingrese el primer número:"));
-  let numero2 = parseInt(prompt("Ingrese el segundo número:"));
-  
-  if (amigos(numero1, numero2)) {
-    console.log(`${numero1} y ${numero2} son números amigos.`);
-  } else {
-    console.log(`${numero1} y ${numero2} no son números amigos .`);
-  }
+ 
 
 
 
@@ -130,17 +133,14 @@ function obtenerTotal(valorUnidad, cantidad) {
       valorTotal = valorTotal*0.90;
     }
   
-     
+  console.log(`El valor subtotal es: ${subtotal}`);
   console.log(`El valor de la compra total es: ${valorTotal}`);
   console.log(`Las cantidades son : ${cantidad}`);
   console.log(`El iva de la transaccion es : ${iva}`);
   }
   
   
-  let precio = parseFloat(prompt("Ingrese el precio del producto: "));
-  let cantidad = parseInt(prompt("Ingrese la cantidad de productos: "));
-  
-  obtenerTotal(precio, cantidad);
+ 
   
 //punto 6
 
@@ -189,27 +189,143 @@ function calcularNotaFinal(nota1, nota2, nota3, nota4) {
     console.log(`Nota 4 ${nota4} `);
   }
   
- let nota1
- let nota2 
- let nota3 
- let nota4 
-  
-  do {
-    nota1 = parseFloat(prompt(`Ingrese la nota numero 1 Debe ser un numero de 0 a 5:`));
-  } while (nota1 < 0 || nota1 > 5);
-  do {
-    nota2 = parseFloat(prompt(`Ingrese la nota numero 2 Debe ser un numero de 0 a 5:`));
-  } while (nota2 < 0 || nota2 > 5);
-  do {
-    nota3 = parseFloat(prompt(`Ingrese la nota numero 3 Debe ser un numero de 0 a 5:`));
-  } while (nota3 < 0 || nota3 > 5);
-  do {
-    nota4 = parseFloat(prompt(`Ingrese la nota numero 4 Debe ser un numero de 0 a 5:`));
-  } while (nota4 < 0 || nota4 > 5);
 
 
+
+
+
+
+  let opcion
+  let contador1 = 0;
+  let contador2 = 0;
+  let contador3 = 0;
+  let contador4 = 0;
+  let contador5 = 0;
+  let contador6 = 0;
+
+
+do {
   
-  let resultado = calcularNotaFinal(nota1, nota2, nota3, nota4);
+  opcion = parseInt(prompt("----- Menú de opciones -----\n1. Ordenar tres números\n2. Calcular área de una circunferencia\n3. Determinar tipo de triángulo\n4. Verificar números amigos\n5. Calcular valor de compra\n6. Calcular notas de un estudiante\n0. Salir o terminar\nIngrese una opción:"));
+
+  if (opcion >= 1 && opcion <= 6) {
+
+    switch (opcion) {
+        case 1:
+            contador1 = contador1 + 1;
+            uno();
+          
+          break;
+        case 2:
+            contador2 = contador2 + 1;
+            dos();
+          break;
+        case 3:
+            contador3 = contador3 + 1;
+            tres();
+          break;
+      
+        case 4:
+            contador4 = contador4 + 1;
+            let numero1 = parseInt(prompt("Ingrese el primer número:"));
+            let numero2 = parseInt(prompt("Ingrese el segundo número:"));
+  
+            if (amigos(numero1, numero2)) {
+                console.log(`${numero1} y ${numero2} son números amigos.`);
+            } else {
+                console.log(`${numero1} y ${numero2} no son números amigos .`);
+            }
+            
+          break;
+        case 5:
+            contador5 = contador5 + 1;
+            let precio = parseFloat(prompt("Ingrese el precio del producto: "));
+            let cantidad = parseInt(prompt("Ingrese la cantidad de productos: "));
+            obtenerTotal(precio, cantidad);
+          break;
+        case 6:
+            contador6 = contador6 + 1;
+            let nota1
+            let nota2 
+            let nota3 
+            let nota4 
+             
+             do {
+               nota1 = parseFloat(prompt(`Ingrese la nota numero 1 Debe ser un numero de 0 a 5:`));
+             } while (nota1 < 0 || nota1 > 5);
+             do {
+               nota2 = parseFloat(prompt(`Ingrese la nota numero 2 Debe ser un numero de 0 a 5:`));
+             } while (nota2 < 0 || nota2 > 5);
+             do {
+               nota3 = parseFloat(prompt(`Ingrese la nota numero 3 Debe ser un numero de 0 a 5:`));
+             } while (nota3 < 0 || nota3 > 5);
+             do {
+               nota4 = parseFloat(prompt(`Ingrese la nota numero 4 Debe ser un numero de 0 a 5:`));
+             } while (nota4 < 0 || nota4 > 5);
+           
+           
+             
+             let resultado = calcularNotaFinal(nota1, nota2, nota3, nota4);
+
+          break;
+      }
+      
+
+    
+  } else if (opcion !== 0) {
+    console.log("Opción inválida. Por favor, intente de nuevo.");
+  }
+} while (opcion !== 0);
+
+
+console.log("----- Cantidad de veces que se ejecuto cada opcion -----");
+console.log(`La opcion 1 se ejecuto: ${contador1} veces`);
+console.log(`La opcion 2 se ejecuto: ${contador2} veces`);
+console.log(`La opcion 3 se ejecuto: ${contador3} veces`);
+console.log(`La opcion 4 se ejecuto: ${contador4} veces`);
+console.log(`La opcion 5 se ejecuto: ${contador5} veces`);
+console.log(`La opcion 6 se ejecuto: ${contador6} veces`);
+
+
+let opciones = ["opcion1", "opcion2", "opcion3", "opcion4", "opcion5", "opcion6"];
+let contadores = [contador1, contador2, contador3, contador4, contador5, contador6];
+let mayor = 0
+let posicionImprimir
+let menor
+
+
+for (let i = 0; i < opciones.length; i++) {
+  
+  
+  if (contadores[i]>mayor) {
+    mayor = contadores[i];
+    posicionImprimir = i;
+  } 
+}
+
+console.log("La opción que más se repitió es:", opciones[posicionImprimir]);
+console.log("Cantidad de repeticiones:", contadores[posicionImprimir]);
+
+menor = contadores[0];
+posicionImprimir = 0;
+
+for (let i = 0; i < opciones.length; i++) {
+  
+  
+    if (contadores[i]<menor) {
+        menor = contadores[i];
+      posicionImprimir = i;
+    } 
+  }
+
+console.log("La opción que menos se repitió es:", opciones[posicionImprimir]);
+console.log("Cantidad de repeticiones:", contadores[posicionImprimir]);
+
+
+
+
+
+
   
       
       
